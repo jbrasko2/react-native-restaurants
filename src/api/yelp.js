0d8d6a1a67ceddc6a073815d982e4a6a0ae11083
Jordan Brasko <jbrasko0508@gmail.com>
2021-08-02 14:13:15 -0500

    Delete yelp.js

--- src/api/yelp.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export default axios.create({
-  baseURL: 'https://api.yelp.com/v3/businesses',
-  headers: {
-    Authorization: 'Bearer TvuONnomZH1c5ltUGfovs-qE9NMQdYPScMwCXrhlajRe_WSO7YGqmvk3sV1ltfZo_bm4JCPDU6T4P-D7fSSe6Ia1GnhZQgQh2VyiGyfb8zBZ8xc3y3OR4DM45wD_YHYx'
-  }
-})
