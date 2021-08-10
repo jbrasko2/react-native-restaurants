981a27ac75efa02c7dfdec5655c5cedb7caf6923
jbrasko <jbrasko0508@gmail.com>
2021-08-10 16:38:42 -0500

    render ResultsDetail image

---
+++ src/components/ResultsDetail.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+
+const ResultsDetail = ({ result }) => {
+  return (
+    <View>
+      <Image source={{ uri: result.image_url }} style={styles.image} />
+      <Text>{result.name}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 250,
+    height: 120,
+    borderRadius: 4,
+  },
+})
+
+export default ResultsDetail
