0d3c6140348ccb8c54f0e6b2967407e4d24ab9aa
jbrasko <jbrasko0508@gmail.com>
2021-08-10 16:17:11 -0500

    add filterResultsByPrice, send as prop

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -8,6 +8,10 @@ const SearchScreen = () => {
   const [term, setTerm] = useState('')
   const [searchApi, results, error] = useResults()
 
+  const filterResultsByPrice = price => {
+    return results.filter(result => result.price === price)
+  }
+
   return (
     <View>
       <Text>Search Screen</Text>
@@ -18,9 +22,9 @@ const SearchScreen = () => {
       />
       {error ? <Text>{error}</Text> : null}
       <Text>We have found {results.length} results.</Text>
-      <ResultsList title='Cost Effective' />
-      <ResultsList title='Bit Pricier' />
-      <ResultsList title='Big Spender' />
+      <ResultsList results={filterResultsByPrice('$')} title='Cost Effective' />
+      <ResultsList results={filterResultsByPrice('$$')} title='Bit Pricier' />
+      <ResultsList results={filterResultsByPrice('$$$')} title='Big Spender' />
     </View>
   )
 }
