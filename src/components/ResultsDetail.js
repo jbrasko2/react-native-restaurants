1be108bea8e2ec80b6a3b67b78201b928b87771e
jbrasko <jbrasko0508@gmail.com>
2021-08-10 16:42:51 -0500

    add margin to container

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -3,18 +3,25 @@ import { StyleSheet, Text, View, Image } from 'react-native'
 
 const ResultsDetail = ({ result }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Image source={{ uri: result.image_url }} style={styles.image} />
-      <Text>{result.name}</Text>
+      <Text style={styles.name}>{result.name}</Text>
+      <Text>{result.rating} Stars, {result.review_count} Reviews</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  container :{
+    marginLeft: 10
+  },
   image: {
     width: 250,
     height: 120,
     borderRadius: 4,
+  },
+  name: {
+    fontWeight: 'bold',
   },
 })
 
