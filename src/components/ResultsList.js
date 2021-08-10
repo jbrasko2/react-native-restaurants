80fc9d68d377d28b68f3758c89f8e649c88313f8
jbrasko <jbrasko0508@gmail.com>
2021-08-10 16:28:43 -0500

    render Flatlist of restaurants

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,20 +1,27 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
 
-const ResultsList = ({title, results}) => {
+const ResultsList = ({ title, results }) => {
   return (
     <View>
       <Text style={styles.title}>{title}</Text>
-      <Text>Results: {results.length}</Text>
+      <FlatList
+        horizontal
+        data={results}
+        keyExtractor={result => result.id}
+        renderItem={({ item }) => {
+          return <Text>{item.name}</Text>
+        }}
+      />
     </View>
   )
 }
 
-const styles = StyleSheet.create({ 
+const styles = StyleSheet.create({
   title: {
     fontSize: 18,
-    fontWeight: 'bold'
-  }
+    fontWeight: 'bold',
+  },
 })
 
 export default ResultsList
